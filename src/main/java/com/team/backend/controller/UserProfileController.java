12484b0384a1641ac1cacab7f82a3d61aef724f6
Kacper Dęga <kacperdgf@o2.pl>
2025-11-03 09:53:55 +0100

    feat: enable CORS for UserProfileController

--- src/main/java/com/team/backend/controller/UserProfileController.java
+++ src/main/java/com/team/backend/controller/UserProfileController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RequestMapping("/profile")
 public class UserProfileController {
